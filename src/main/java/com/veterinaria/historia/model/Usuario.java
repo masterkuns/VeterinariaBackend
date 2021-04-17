9dc8533b39e6ff5cfbaed6c763f961e5fac4fb15
yaca <angeldav99@hotmail.com>
2021-04-16 22:04:36 -0500

    arrglando el mapeo
    finalizando mapeo

--- src/main/java/com/veterinaria/historia/model/Usuario.java
+++ src/main/java/com/veterinaria/historia/model/Usuario.java
@@ -1,35 +1,36 @@
 package com.veterinaria.historia.model;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table (name ="usuario")
+@Table(name = "usuario")
 public class Usuario {
 	@Id
-	
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-	  private Integer id;
-	  private String nombre;
-	  private String apellido;
-	  @Column(name="tipo_Identificacion")
-	  private String tipoDocumento;
-	  @Column(name="documento_Identificacion")
-	  private Integer documentoIdentificacion;
-	  private String estado;
-	  private String sexo;
-	  
-	  
-	  
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private String apellido;
+	@Column(name = "tipo_documento")
+	private String tipoDocumento;
+
+	@Column(unique = true, name = "documento_Identificacion")
+
+	private Integer documentoIdentificacion;
+	private String estado;
+	private String sexo;
+
 	public Usuario() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Usuario(String nombre, String apellido, String tipoDocumento, Integer documentoIdentificacion, String estado,
 			String sexo) {
 		super();
@@ -40,45 +41,59 @@ public class Usuario {
 		this.estado = estado;
 		this.sexo = sexo;
 	}
+
 	public Integer getid() {
 		return id;
 	}
+
 	public void setid(Integer id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getTipoDocumento() {
 		return tipoDocumento;
 	}
+
 	public void setTipoDocumento(String tipoDocumento) {
 		this.tipoDocumento = tipoDocumento;
 	}
+
 	public Integer getDocumentoIdentificacion() {
 		return documentoIdentificacion;
 	}
+
 	public void setDocumentoIdentificacion(Integer documentoIdentificacion) {
 		this.documentoIdentificacion = documentoIdentificacion;
 	}
+
 	public String getEstado() {
 		return estado;
 	}
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
+
 	public String getSexo() {
 		return sexo;
 	}
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
--- src/main/java/com/veterinaria/historia/rest/UsuarioRest.java
+++ src/main/java/com/veterinaria/historia/rest/UsuarioRest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class UsuarioRest {
 				
 				
 	}
-	
+	@PostMapping
 	private ResponseEntity<Usuario>saveUsuario(@RequestBody Usuario  usuario){
 		try {
 			 Usuario usuarioGuardado=usuarioService.save(usuario);
