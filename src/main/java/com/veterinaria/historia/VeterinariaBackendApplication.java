5cfc33836b61938825ee2ca1b9c143d2ab66cffa
yaca <angeldav99@hotmail.com>
2021-04-18 20:14:31 -0500

    make it better

--- src/main/java/com/veterinaria/historia/VeterinariaBackendApplication.java
+++ src/main/java/com/veterinaria/historia/VeterinariaBackendApplication.java
@@ -25,7 +25,7 @@ public class VeterinariaBackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://veterinariafront.herokuapp.com/").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://veterinariafront.herokuapp.com").allowedMethods("*").allowedHeaders("*");
 				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
 			}
 			
