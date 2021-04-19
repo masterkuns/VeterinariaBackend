ea3ac05fcd2ab6997787ac1a747b592cbce21622
yaca <angeldav99@hotmail.com>
2021-04-18 20:31:40 -0500

    make it better

--- src/main/java/com/veterinaria/historia/VeterinariaBackendApplication.java
+++ src/main/java/com/veterinaria/historia/VeterinariaBackendApplication.java
@@ -27,17 +27,13 @@ public class VeterinariaBackendApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("https://veterinariafront.herokuapp.com").allowedMethods("*").allowedHeaders("*");
 				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT","DELETE");
 			}
 			
 			
-			public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		        HttpServletResponse response = (HttpServletResponse) res;
-		        response.setHeader("Access-Control-Allow-Origin", "*");
-		        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-		        response.setHeader("Access-Control-Max-Age", "3600");
-		        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-		        chain.doFilter(req, res);
-			}
+			
 		};
 	}
 
--- src/main/java/com/veterinaria/historia/rest/UsuarioRest.java
+++ src/main/java/com/veterinaria/historia/rest/UsuarioRest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.veterinaria.historia.model.Usuario;
 import com.veterinaria.historia.service.UsuarioService;
 
+@CrossOrigin (origins = "https://veterinariaprueba.herokuapp.com/usuario/")
 @RestController
 @RequestMapping("/usuario/")
 public class UsuarioRest {
