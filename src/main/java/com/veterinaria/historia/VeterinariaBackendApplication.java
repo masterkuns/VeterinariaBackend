b97b19d62610bd3b33622412e7c8712e4e23849c
yaca <angeldav99@hotmail.com>
2021-04-18 20:00:27 -0500

    make it better

--- src/main/java/com/veterinaria/historia/VeterinariaBackendApplication.java
+++ src/main/java/com/veterinaria/historia/VeterinariaBackendApplication.java
@@ -1,5 +1,13 @@
 package com.veterinaria.historia;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +27,16 @@ public class VeterinariaBackendApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**").allowedOrigins("https://veterinariafront.herokuapp.com").allowedMethods("*").allowedHeaders("*");
 				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+			
+			
+			public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		        HttpServletResponse response = (HttpServletResponse) res;
+		        response.setHeader("Access-Control-Allow-Origin", "*");
+		        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+		        response.setHeader("Access-Control-Max-Age", "3600");
+		        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+		        chain.doFilter(req, res);
 			}
 		};
 	}
